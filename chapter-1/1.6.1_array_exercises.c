4f9fb8538d453871374b5c1f9d3dde19f1ff2865
Josh Freeman <jdfreeman@satx.rr.com>
2014-07-11 15:42:54 -0500

    Add Initial 1.6.1 exercises

---
+++ chapter-1/1.6.1_array_exercises.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * Execute in termial and then type and press enter
+ */
+void main()
+{
+    exercise1_13();
+}
+
+
+#define MAX_WORD_LENGTH 20
+
+/**
+ * Write a program to print a histogram of the frequencies of
+ * different characters in its input.
+ */
+void exercise1_13()
+{
+    int c;
+    int unique_characters;
+    char character_count[], characters_added[];
+
+    while ((c = getchar()) != EOF)
+        characters_added = c;
+        ++character_count[c];
+
+}
